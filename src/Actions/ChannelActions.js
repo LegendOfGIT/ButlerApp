f1b5ffa8d138305ed3bed5eb9bbb196d21a30b7c
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2017-08-16 23:15:47 +0200

    - fixed unit tests

--- src/Actions/ChannelActions.js
+++ src/Actions/ChannelActions.js
@@ -1,21 +1,53 @@
 import 'isomorphic-fetch';
 import {
-    FETCH_CHANNEL_DATA_SUCCESS
+    FETCH_CHANNEL_DATA_SUCCESS,
+    FETCH_CHANNEL_HAS_ERROR,
+    FETCH_CHANNEL_IS_LOADING
 } from './ActionTypes';
+
+function fetchChannelHasError(hasError) {
+    return {
+        type: FETCH_CHANNEL_HAS_ERROR,
+        payload: hasError
+    }
+}
+function fetchChannelIsLoading(isLoading) {
+    return {
+        type: FETCH_CHANNEL_IS_LOADING,
+        payload: isLoading
+    }
+}
+function fetchChannelDataSuccess(channel) {
+    return {
+        type: FETCH_CHANNEL_DATA_SUCCESS,
+        payload: channel
+    }
+}
 
 export function fetchChannelData()
 {
     return (dispatch) => {
+        dispatch(fetchChannelHasError(false));
+        dispatch(fetchChannelIsLoading(true));
         return fetch('http://127.0.0.1:12354/InformationChannelService/GetChannel')
             .then((response) => {
+                if (!response.ok) {
+                    throw Error('Service error');
+                }
+
+                return response;
+            })
+            .then((response) => {
+                dispatch(fetchChannelIsLoading(false));
+
                 const jsonResponse = response.json();
                 return jsonResponse;
             })
             .then((channel) =>{
-                dispatch({
-                    type:  FETCH_CHANNEL_DATA_SUCCESS,
-                    payload: channel
-                });
+                dispatch(fetchChannelDataSuccess(channel));
+            })
+            .catch(() => {
+                dispatch(fetchChannelHasError(true));
             });
     }
 }
--- src/Actions/ChannelActions.test.js
+++ src/Actions/ChannelActions.test.js
@@ -1,5 +1,9 @@
 import { fetchChannelData } from './ChannelActions';
-import { FETCH_CHANNEL_DATA_SUCCESS } from './ActionTypes';
+import {
+    FETCH_CHANNEL_DATA_SUCCESS,
+    FETCH_CHANNEL_HAS_ERROR,
+    FETCH_CHANNEL_IS_LOADING
+} from './ActionTypes';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
@@ -7,12 +11,13 @@ const middleware = [thunk];
 const mockStore = configureStore(middleware);
 const store = mockStore();
 
-const httpCodes = {
-    success: 200,
-    internalServerError: 500
-};
+global.fetch = require('jest-fetch-mock');
 
 describe('Channel Actions', () => {
+    beforeEach(() => {
+        fetch.resetMocks();
+    });
+
     it('calls action ' + FETCH_CHANNEL_DATA_SUCCESS + ' with response in payload, when the fetch response was successful', () => {
         const payload = {
             channelItems: [
@@ -23,17 +28,37 @@ describe('Channel Actions', () => {
             title: "Aktivitäten in Hannover"
         };
 
-        window.fetch =
-            jest.fn().mockImplementation(
-                () => Promise.resolve({
-                    json: () => Promise.resolve(payload)
-                })
-            );
+        fetch.mockResponseOnce(JSON.stringify(payload));
 
         store.dispatch(fetchChannelData()).then(() => {
             const triggeredActions = store.getActions();
             expect(triggeredActions).toEqual([
+                { type: FETCH_CHANNEL_HAS_ERROR, payload: false },
+                { type: FETCH_CHANNEL_IS_LOADING, payload: true },
+                { type: FETCH_CHANNEL_IS_LOADING, payload: false },
                 { type: FETCH_CHANNEL_DATA_SUCCESS, payload: payload }
+            ]);
+        });
+    });
+
+    it('calls action ' + FETCH_CHANNEL_HAS_ERROR + ', when the fetch response was unsuccessful', () => {
+        const payload = {
+            channelItems: [
+                { id: "AAA-BBB-CCC" },
+                { id: "BBB-CCC-DDD" },
+                { id: "CCC-DDD-EEE"}
+            ],
+            title: "Aktivitäten in Hannover"
+        };
+
+        fetch.mockRejectOnce();
+
+        store.dispatch(fetchChannelData()).then().catch(() => {
+            const triggeredActions = store.getActions();
+            expect(triggeredActions).toEqual([
+                { type: FETCH_CHANNEL_HAS_ERROR, payload: false },
+                { type: FETCH_CHANNEL_IS_LOADING, payload: true },
+                { type: FETCH_CHANNEL_HAS_ERROR, payload: true }
             ]);
         });
     });
