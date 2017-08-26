46ca11f20033b5ae30c6e8061482f1e7e480bb84
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2017-08-26 23:35:26 +0200

    - added action "fetchChannelItemData(itemId)

--- src/Actions/ChannelActions.js
+++ src/Actions/ChannelActions.js
@@ -2,7 +2,10 @@ import 'isomorphic-fetch';
 import {
     FETCH_CHANNEL_DATA_SUCCESS,
     FETCH_CHANNEL_HAS_ERROR,
-    FETCH_CHANNEL_IS_LOADING
+    FETCH_CHANNEL_IS_LOADING,
+    FETCH_CHANNEL_ITEM_DATA_SUCCESS,
+    FETCH_CHANNEL_ITEM_HAS_ERROR,
+    FETCH_CHANNEL_ITEM_IS_LOADING
 } from './ActionTypes';
 
 function fetchChannelHasError(hasError) {
@@ -48,6 +51,58 @@ export function fetchChannelData()
             })
             .catch(() => {
                 dispatch(fetchChannelHasError(true));
+            });
+    }
+}
+
+function fetchChannelItemHasError(itemId, hasError) {
+    return {
+        type: FETCH_CHANNEL_ITEM_HAS_ERROR,
+        payload: {
+            itemId: itemId,
+            hasError: hasError
+        }
+    }
+}
+function fetchChannelItemIsLoading(itemId, isLoading) {
+    return {
+        type: FETCH_CHANNEL_ITEM_IS_LOADING,
+        payload: {
+            itemId: itemId,
+            isLoading: isLoading
+        }
+    }
+}
+function fetchChannelItemDataSuccess(channelItem) {
+    return {
+        type: FETCH_CHANNEL_ITEM_DATA_SUCCESS,
+        payload: channelItem
+    }
+}
+
+export function fetchChannelItemData(itemId)
+{
+    return (dispatch) => {
+        dispatch(fetchChannelItemHasError(itemId, false));
+        dispatch(fetchChannelItemIsLoading(itemId, true));
+        return fetch('http://127.0.0.1:12354/InformationChannelService/GetChannelItem/' + itemId)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error('Service error');
+                }
+
+                return response;
+            })
+            .then((response) => {
+                dispatch(fetchChannelItemIsLoading(itemId, false));
+
+                const jsonResponse = response.json();
+                return jsonResponse;
+            })
+            .then((channelItem) =>{
+                dispatch(fetchChannelItemDataSuccess(channelItem));
+            }).catch(() => {
+                dispatch(fetchChannelItemHasError(itemId,true));
             });
     }
 }
--- src/Actions/ChannelActions.test.js
+++ src/Actions/ChannelActions.test.js
@@ -1,21 +1,25 @@
-import { fetchChannelData } from './ChannelActions';
+import { fetchChannelData, fetchChannelItemData } from './ChannelActions';
 import {
     FETCH_CHANNEL_DATA_SUCCESS,
     FETCH_CHANNEL_HAS_ERROR,
-    FETCH_CHANNEL_IS_LOADING
+    FETCH_CHANNEL_IS_LOADING,
+    FETCH_CHANNEL_ITEM_DATA_SUCCESS,
+    FETCH_CHANNEL_ITEM_HAS_ERROR,
+    FETCH_CHANNEL_ITEM_IS_LOADING
 } from './ActionTypes';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
 const middleware = [thunk];
 const mockStore = configureStore(middleware);
-const store = mockStore();
+let store;
 
 global.fetch = require('jest-fetch-mock');
 
 describe('Channel Actions', () => {
     beforeEach(() => {
         fetch.resetMocks();
+        store = mockStore();
     });
 
     describe('Action fetchChannelData', () => {
@@ -57,7 +61,41 @@ describe('Channel Actions', () => {
     });
 
     describe('Action fetchChannelItem', () => {
+        it('calls action ' + FETCH_CHANNEL_ITEM_DATA_SUCCESS + ' with response in payload, when the fetch response was successful', () => {
+            const itemId = 'AAA-BBB-CCC-DDD';
+            const payload = {
+                itemId: itemId,
+                description: 'Channel item description',
+                header: 'Channel item header',
+                title: 'Channel item title'
+            };
 
+            fetch.mockResponseOnce(JSON.stringify(payload));
+
+            store.dispatch(fetchChannelItemData(itemId)).then(() => {
+                const triggeredActions = store.getActions();
+                expect(triggeredActions).toEqual([
+                    { type: FETCH_CHANNEL_ITEM_HAS_ERROR, payload: { itemId: itemId, hasError: false } },
+                    { type: FETCH_CHANNEL_ITEM_IS_LOADING, payload: { itemId: itemId, isLoading: true } },
+                    { type: FETCH_CHANNEL_ITEM_IS_LOADING, payload: { itemId: itemId, isLoading: false } },
+                    { type: FETCH_CHANNEL_ITEM_DATA_SUCCESS, payload: payload }
+                ]);
+            });
+        });
+
+        it('calls action ' + FETCH_CHANNEL_ITEM_HAS_ERROR + ', when the fetch response was unsuccessful', () => {
+            const itemId = 'AAA-BBB-CCC-DDD';
+            fetch.mockRejectOnce();
+
+            store.dispatch(fetchChannelItemData(itemId)).then().catch(() => {
+                const triggeredActions = store.getActions();
+                expect(triggeredActions).toEqual([
+                    { type: FETCH_CHANNEL_ITEM_HAS_ERROR, payload: { itemId: itemId, hasError: false } },
+                    { type: FETCH_CHANNEL_ITEM_IS_LOADING, payload: { itemId: itemId, isLoading: true } },
+                    { type: FETCH_CHANNEL_ITEM_HAS_ERROR, payload: { itemId: itemId, hasError: true } }
+                ]);
+            });
+        });
     });
 });
 
